43057d8812e0843a254c25a53832063de96c98fe
BooLeanAtYourService <123937716+BooLeanAtYourService@users.noreply.github.com>
2024-09-14 10:54:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+  const perfectMan = document.getElementById('perfectMan');
+  let isMoving = false;
+
+  // Function to start the movement
+  window.startAnimation = function() {
+    if (!isMoving) {
+      perfectMan.classList.add('move');
+      isMoving = true;
+    }
+  }
+
+  // Function to stop the movement
+  window.stopAnimation = function() {
+    if (isMoving) {
+      perfectMan.classList.remove('move');
+      isMoving = false;
+    }
+  }
+});
